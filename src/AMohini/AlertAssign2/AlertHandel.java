5f6971720f3717653b4bff7222f551e7e5336410
MohiniAgarwal <mohinih.agarwal@gmail.com>
2022-10-27 23:25:36 +0530

    MohiniA_Sel_Assignment2

---
+++ src/AMohini/AlertAssign2/AlertHandel.java
@@ -0,0 +1,70 @@
+
+/********Assignment No 2 : 27th Oct 2022
+
+Test Case: To Handle Alert & Validate that "You successfully clicked on it" message is getting display in the alert.
+
+1. Launch chrome browser
+2. Hit url(http://automationbykrishna.com/)in browser.
+3. Click on basic element tab
+4. Enter Email address
+5. Enter password
+6. Click on the submit button
+7. Validate that "You successfully clicked on it" message is displayed
+8. Accept the alert
+ * *********/
+
+package AMohini.AlertAssign2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import AMohini.CommonActions.*;
+
+public class AlertHandel {
+
+	public static void main(String[] args) throws Exception {
+
+		System.out.println("STEP - Launch Chrome browser & load URL");
+		WebDriver driver = PredefinedActions.start("http://automationbykrishna.com/");
+		/*****
+		 * driver.findElement(By.id("firstname")).sendKeys("Mohini");
+		 * driver.findElement(By.id("lastnamename")).sendKeys("Agarwal");
+		 * 
+		 * driver.findElement(By.id("female")).click();
+		 ***********/
+
+		driver.findElement(By.id("basicelements")).click();
+		Thread.sleep(1000);
+		String emailAddress = "Mohinih.agarwal@gmail.com";
+		String password = "Apple@123";
+		driver.findElement(By.id("exampleInputEmail1")).sendKeys(emailAddress);
+		Thread.sleep(1000);
+		driver.findElement(By.id("pwd")).sendKeys(password);
+		/******
+		 * driver.findElement(By.id("h4")).click();
+		 * 
+		 * WebElement e = driver.findElement(By.id("bId")); Select select1 = new
+		 * Select(e);
+		 * 
+		 * select1.selectByVisibleText("IT");
+		 *********/
+
+		driver.findElement(By.id("submitb2")).click();
+		Thread.sleep(1000);
+		String Expected = "You successfully clicked on it";
+		String Actual = driver.switchTo().alert().getText();
+		if (Expected.equals(Actual)) {
+			System.out.println("Test Case Passed");
+
+		} else {
+			System.out.println("Testcase Failed");
+		}
+		driver.switchTo().alert().accept();
+		Thread.sleep(2);
+		driver.quit();
+
+	}
+
+}
--- src/AMohini/Assignment1/HTMLFormAssignment1.java
+++ src/AMohini/Assignment1/HTMLFormAssignment1.java
@@ -1,7 +1,18 @@
-
-
+/**********Assignment No 1 : 
+	Test Cases: 
+		1.Launch chrome browser
+		2.Hit url(html page created)in browser.
+		3.Verify application is loaded successfully.
+		4.Enter First Name
+		5.Enter Last Name
+		6.Select gender radio button (at a time only one radio button should be selected)
+		7.Select Hobbies checkbox (user can select multiple checkboxes)
+		8.Select value from branch dropdown.
+		9.Click on Click here link should redirect to url configured (eg. https://www.google.com).
+		10.Navigate back to the previous url.
+		11.Verify html page is loaded again.
+************/
 package AMohini.Assignment1;
-
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,30 +22,25 @@ import org.openqa.selenium.support.ui.Select;
 import AMohini.CommonActions.*;
 
 public class HTMLFormAssignment1 {
-	
-	
-	
-		
-		public static void main(String[] args) {
-			
-			System.out.println("STEP - Launch Chrome browser & load URL");
-			WebDriver driver= PredefinedActions.start("file:///C:/Users/mohin/JAVACourse/SeleniumTechnoAug2022/src/AMohini/HTML/MyFirst.html");
-			driver.findElement(By.id("firstname")).sendKeys("Mohini");
-			driver.findElement(By.id("lastnamename")).sendKeys("Agarwal");
-			
-			driver.findElement(By.id("female")).click();
-			
-			driver.findElement(By.id("h3")).click();
-			driver.findElement(By.id("h4")).click();
-			
-			WebElement e =driver.findElement(By.id("bId"));
-			Select select1 = new Select(e); 
-			
-			select1.selectByVisibleText("IT");
-			
-			
-			
 
-}
-		
+	public static void main(String[] args) {
+
+		System.out.println("STEP - Launch Chrome browser & load URL");
+		WebDriver driver = PredefinedActions
+				.start("file:///C:/Users/mohin/JAVACourse/SeleniumTechnoAug2022/src/AMohini/HTML/MyFirst.html");
+		driver.findElement(By.id("firstname")).sendKeys("Mohini");
+		driver.findElement(By.id("lastnamename")).sendKeys("Agarwal");
+
+		driver.findElement(By.id("female")).click();
+
+		driver.findElement(By.id("h3")).click();
+		driver.findElement(By.id("h4")).click();
+
+		WebElement e = driver.findElement(By.id("bId"));
+		Select select1 = new Select(e);
+
+		select1.selectByVisibleText("IT");
+
+	}
+
 }
