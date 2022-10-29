e0be4152ad083a38287777c501f1ae1b26b0c8c5
Vibha Sawner <vibha23sawner@gmail.com>
2022-10-29 20:18:15 +0530

    modified prdefinedAction class

--- src/vibha/PredifinedAction.java
+++ src/vibha/PredifinedAction.java
@@ -1,16 +1,39 @@
 package vibha;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
 
 public class PredifinedAction {
 	
-	 final static public WebDriver start() {
+	private static WebDriver driver;
+	
+	final static public WebDriver start() {
+		return start("http://automationbykrishna.com");
+		
+	}
+	
+	 final static public WebDriver start(String url) {
 		System.out.println("Step 1 --> Launch crome browser");
 		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver_106.exe");
 		WebDriver driver=new ChromeDriver();
-		driver.get("");
+		driver.get("url");
+		driver.manage().window().maximize();
 		return driver;
 	}
 
+	 final public static void verifyTitle(String expectedTitle) {
+		 String actualTitle=driver.getTitle();
+		 if(expectedTitle.equals(actualTitle)) {
+			 System.out.println("pass");
+		 }else {
+			 System.out.println("fail");
+		 }
+	 }
+	 
+	 final public static void scrollviewToElement(WebElement element) {
+		 JavascriptExecutor je=(JavascriptExecutor)driver;
+		 je.executeScript("arguments[0].scrollIntoView", element);
+	 }
 }
