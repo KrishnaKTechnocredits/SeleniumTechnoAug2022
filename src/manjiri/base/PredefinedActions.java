7c3722cc4eea6143d9f8dfd9466cce45ad4feac6
ManjiriC0406 <manjiri.chourikar@gmail.com>
2022-10-29 22:29:56 +0530

    updated PreDefinedActions class

--- src/manjiri/base/PredefinedActions.java
+++ src/manjiri/base/PredefinedActions.java
@@ -1,9 +1,14 @@
 package manjiri.base;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class PredefinedActions {
+	
+	private static WebDriver driver = null;
+	
 	final static public WebDriver start() {
 		return start("https://automationpractice.com/");
 	}
@@ -11,9 +16,22 @@ public class PredefinedActions {
 	final static public WebDriver start(String url) {
 		System.out.println("STEP - Launch Chrome Browser");
 		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver_106.exe");
-		WebDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		driver.get(url);
 		driver.manage().window().maximize();
 		return driver;
+	}
+	
+	final static public void closeBrowser() {
+		driver.close();
+	}
+	
+	final static public void closeAllBrowser() {
+		driver.quit();
+	}
+	
+	final static public void scrollToElement(WebElement element) {
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		js.executeScript("arguments[0].scrollIntoView(true)", element);
 	}
 }
