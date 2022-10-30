82515136dbbd53fbe5e34b55a1b1951f7ceef7d8
Krishna Kanani <kananikrishna427@gmail.com>
2022-10-30 08:16:29 +0530

    Session Code

---
+++ src/technocredits/PredefinedActions.java
@@ -0,0 +1,36 @@
+package technocredits;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PredefinedActions {
+	private static WebDriver driver;
+	
+	static public WebDriver start() {
+		return start("https://automationpractice.com");
+	}
+	
+	static public WebDriver start(String url) {
+		System.out.println("STEP - Launch Chrome Browser");
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver_106.exe");
+		driver = new ChromeDriver();
+		driver.get(url);
+		driver.manage().window().maximize();
+		return driver;
+	}
+	
+	static void scrollToElement(WebElement element){
+		JavascriptExecutor je = (JavascriptExecutor)driver;
+		je.executeScript("arguments[0].scrollIntoView(true)", element);
+	}
+	
+	static void closeBrowser() {
+		driver.close();
+	}
+	
+	static void closeAllBrowser() {
+		driver.quit();
+	}
+}
