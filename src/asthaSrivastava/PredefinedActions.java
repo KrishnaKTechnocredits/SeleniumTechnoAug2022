91d2fcb388d44a93c2d0ce8e41af98f38e1d2caa
Asthasrivas <astha.srivas1992@gmail.com>
2022-11-09 02:03:09 +0530

    Assignment8_Astha

--- src/asthaSrivastava/PredefinedActions.java
+++ src/asthaSrivastava/PredefinedActions.java
@@ -1,23 +1,37 @@
 package asthaSrivastava;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class PredefinedActions {
 	
-	final static public WebDriver start() {
-		return start("file:///E:/Java2022/Assignments/Selenium/FirstHtmlPage.html");
-	}
-	
-	final static public WebDriver start(String url) {
-		System.setProperty("webdriver.chrome.driver","src\\driver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+	private static WebDriver driver;
 		
-		driver.get(url);
-		driver.manage().window().maximize();
-		return driver;
-	}
-	public static void main(String[] args) {
+		static public WebDriver start() {
+			return start("http://automationbykrishna.com");
+		}
 		
+		static public WebDriver start(String url) {
+			System.out.println("STEP - Launch Chrome Browser");
+			System.setProperty("webdriver.chrome.driver", "drivers/chromedriver_106.exe");
+			driver = new ChromeDriver();
+			driver.get(url);
+			driver.manage().window().maximize();
+			return driver;
+		}
+		
+		static public void scrollToElement(WebElement element){
+			JavascriptExecutor je = (JavascriptExecutor)driver;
+			je.executeScript("arguments[0].scrollIntoView(true)", element);
+		}
+		
+		static public void closeBrowser() {
+			driver.close();
+		}
+		
+		static public void closeAllBrowser() {
+			driver.quit();
+		}
 	}
-}
